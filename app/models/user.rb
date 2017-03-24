54b0f24b30e522be5dafc754bcb79c9f54c1345c
klawton1 <klawton1@gmail.com>
2017-03-23 19:29:35 -0700

    fixed avatar issue

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   has_many :playdates
   has_many :dogs
   has_many :invites
+  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "assets/images/doge-default.png"
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 end
