1cff04155690176d2fb4b4d8b524efd50fa5ec65
rccacho <rccacho@gmail.com>
2017-03-23 17:03:51 -0700

    changed integer to status to fix the error

--- db/seeds.rb
+++ db/seeds.rb
@@ -117,20 +117,20 @@ apollo_pals = Playdate.create({
 ein_foo = Invite.create ({
 	user_id: regelyn.id,
 	playdate_id: ein_pals.id,
-	integer: 1
+	status: 1
 	})
 apollo_koda = Invite.create ({
 	user_id: kody.id,
 	playdate_id: apollo_pals.id,
-	integer: 1
+	status: 1
 	})
 koda_foo = Invite.create ({
 	user_id: yvonne.id,
 	playdate_id: koda_pals.id,
-	integer: 1
+	status: 1
 	})
 foo_apollo = Invite.create ({
 	user_id: doge.id,
 	playdate_id: foo_pals.id,
-	integer: 1
+	status: 1
 	})
