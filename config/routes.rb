3a24c926ad3b77e44d1b36f3fe198a54daad2a68
rccacho <rccacho@gmail.com>
2017-03-24 13:55:44 -0700

    got all dogs to show up on the dogs#index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   root to: 'users#home'
 
+  get '/dogs', to: 'dogs#index', as: 'dogs'
+
   resources :users do
     resources :playdates, shallow: true
-    resources :dogs, shallow: true
+    resources :dogs, shallow: true, except: (:index)
   end
 
   get     '/login',    to: 'sessions#new'
