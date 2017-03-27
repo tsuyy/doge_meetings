cdf8114a2fb984dea82647076e8dd1ae90fe9d03
kkuo57 <kkuo507@gmail.com>
2017-03-27 16:52:09 -0700

    fixes playdate destroy and user destroy

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
-  has_many :playdates
-  has_many :dogs
-  has_many :invites
+  has_many :playdates, dependent: :destroy
+  has_many :dogs, dependent: :destroy
+  has_many :invites, dependent: :destroy
   has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "assets/images/doge-default.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 end
