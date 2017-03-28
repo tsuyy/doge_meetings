b123ae7075a4a1b749ff3f2842acca9ab89b948e
rccacho <rccacho@gmail.com>
2017-03-28 13:49:44 -0700

    add test for user show and gem for controller testing

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,11 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, :type => :controller do
-  describe "::new" do
+  describe "GET #new" do
     it "initializes a new user" do
       user = User.new
       expect(user).to be_a(User)
     end
   end
-  
+
+  describe "GET #show" do
+    it "responds successfully with an HTTP 200 status code" do
+      get :show
+      expect(response).to be_success
+      expect(response).to have_http_status(200)
+    end
+    it "renders the #show view" do
+      get :show, id: FactoryGirl.build(:user)
+      expect(response).to render_template :show
+    end
+  end
+
 end
