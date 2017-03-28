f4122a1cc45651ba58fd1c5873b9ebd9613a82cd
kkuo57 <kkuo507@gmail.com>
2017-03-27 19:58:03 -0700

    fixes presence for password

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,5 +8,5 @@ class User < ApplicationRecord
   validates :email, uniqueness: true, presence: true
   validates :name, presence: true
   validates :city, presence: true
-  validates :password_digest, presence: true
+  validates :password, presence: true
 end
