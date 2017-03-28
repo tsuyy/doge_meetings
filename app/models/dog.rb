b2054d860ac8a0da7eab4116fb3f73b39992cb41
kkuo57 <kkuo507@gmail.com>
2017-03-27 19:44:00 -0700

    adds unique email, no only whitespace, some flash messages

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -2,4 +2,11 @@ class Dog < ApplicationRecord
   belongs_to :user
   has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "assets/images/doge-default.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
+  validates :name, presence: true
+  validates :age, presence: true
+  validates :sex, presence: true
+  validates :breed, presence: true
+  validates :vaccinated, presence: true
+  validates :neutered, presence: true
+  validates :weight, presence: true
 end
