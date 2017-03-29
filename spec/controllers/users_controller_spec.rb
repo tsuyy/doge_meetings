8af69f6c2cbecd6efa4d33f61560e27b366c1509
rccacho <rccacho@gmail.com>
2017-03-28 17:21:53 -0700

    add more tests for users controller

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -10,14 +10,67 @@ RSpec.describe UsersController, :type => :controller do
 
   describe "GET #show" do
     it "responds successfully with an HTTP 200 status code" do
-      get :show
-      expect(response).to be_success
+      user = FactoryGirl.create(:user)
+      get :show, id: user.id
       expect(response).to have_http_status(200)
     end
     it "renders the #show view" do
-      get :show, id: FactoryGirl.build(:user)
+      user = FactoryGirl.create(:user)
+      get :show, id: user.id
       expect(response).to render_template :show
     end
+    it "redirects to login_path if not logged in" do
+      user = FactoryGirl.create(:user)
+      User.stub(:find).and_return(user)
+      get :show, id: user
+      expect(response).to redirect_to(login_path)
+    end
+  end
+
+  describe "GET #edit" do
+    it "responds successfully with an HTTP 200 status code" do
+      user = FactoryGirl.create(:user)
+      get :edit, id: user.id
+      expect(response).to have_http_status(200)
+    end
+    it "renders the #edit view" do
+      user = FactoryGirl.create(:user)
+      User.stub(:find).and_return(user)
+      get :edit, id: user
+      expect(response).to render_template :edit
+    end
+    it "redirects to login_path if not current_user" do
+      user = FactoryGirl.create(:user)
+      User.stub(:find).and_return(user)
+      get :edit, id: user
+      expect(response).to redirect_to(login_path)
+    end
+  end
+
+  describe "POST #create" do
+    context "with valid attributes" do
+      it "creates a new user" do
+        expect{
+          post :create, user: FactoryGirl.attributes_for(:user)
+        }.to change(User,:count).by(1)
+      end
+      
+      it "redirects to the new user" do
+        post :create, user: FactoryGirl.attributes_for(:user)
+        response.should redirect_to User.last
+      end
+    end
+    context "with invalid attributes" do
+      it "does not save the new user" do
+        expect{
+          post :create, user: FactoryGirl.attributes_for(:invalid_user)
+        }.to_not change(User,:count)
+      end
+      it "re-renders the new method" do
+        post :create, user: FactoryGirl.attributes_for(:invalid_user)
+        response.should render_template :new
+      end
+    end 
   end
 
 end
